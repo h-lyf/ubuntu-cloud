05c0d3e91c1727094cf3141aa3e39732a54751d0
h-lyf <h.lianyufeng@gmail.com>
2025-08-01 03:14:41 +0800

    modified:   start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ if [ -n "$PASSWD" ]; then
 fi
 
 # 生成最终配置文件
-mkdir -p /root/data
+mkdir -p /root/data /root/data/xray /root/data/sing-box
 if [ ! -f /root/data/xray/config.json ]; then
   envsubst < /etc/xray/config.template > /root/data/xray/config.json
 fi
