43df073b05b259ac3c987f3404adb3f7e0ee1ca8
h-lyf <h.lianyufeng@gmail.com>
2025-08-01 12:03:36 +0800

    modified:   start.sh 	modified:   supervisord.conf

--- start.sh
+++ start.sh
@@ -10,6 +10,3 @@ envsubst < /etc/xray/config.template > /etc/xray/config.json
 
 # 启动 supervisord 管理服务
 exec /usr/bin/supervisord -c /etc/supervisord.conf
-
-# 启动 openlist
-nohup /usr/local/bin/openlist server > /var/log/openlist.out.log 2> /var/log/openlist.err.log &
